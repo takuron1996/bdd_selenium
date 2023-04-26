a9d0ba09b53d13325c1d793b921f12f2142d9302
taku_ikegami <takuron1996@gmail.com>
2023-04-26 09:39:46 +0900

    初期環境

---
+++ application/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf_8 -*-
+"""実行スクリプト
+"""
+
+__author__ = "Taku Ikegami"
+__version__ = "1.0.1"
+__date__ = "2023/04/26(Created: 2023/04/26)"
+
+from bdd_selenium.example import main
+
+if __name__ == "__main__":
+
+    main()
---
+++ application/tests/test_example.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf_8 -*-
+"""例題プログラム
+"""
+
+__author__ = 'Taku Ikegami'
+__version__ = '1.0.1'
+__date__ = '2023/04/26(Created: 2023/04/26)'
+
+from bdd_selenium import example
+
+
+def test_main():
+    """例題プログラムのテスト
+    """
+    assert example.main() == "HelloWorld"
+
